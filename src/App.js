e4c5415c3d1c91ddfe6dc01fad1aaa85c23bc1e2
Aili ShivaPrasad <112164679+Ailishiva@users.noreply.github.com>
2023-05-07 13:41:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
 
-import React from 'react';
-import Contact from './Components/Contact';
-// import { Provider } from 'react-redux';
-// import ContactForm from './Components/ContactForm';
-// import ContactList from './Components/ContactList';
-// import store from './Components/store';
-// import Dashboard from './Map/Dashboard';
-// import CountryCasesMap from './Map/CounterCaseMap';
+import './App.css';
+import PhoneVerification from './Component/PhoneVerification';
 
-const App = () => {
+function App() {
   return (
-    <>
-    <Contact/>
-    </>
-
-    // <>
-    // <Provider store={store}>
-    //   <div>
-    //     <h1>Contact Management App</h1>
-    //     <ContactForm />
-    //     <ContactList />
-    //   </div>
-    // </Provider>
-    // <h1>Map</h1>
-    // <Dashboard/>
-    // <h1>CountryCasesMap</h1>
-    // <CountryCasesMap/>
-    // </>
-    
+    <div className="App">
+      <PhoneVerification/>
+    </div>
   );
-};
+}
 
 export default App;
